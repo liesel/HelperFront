e647a6e4b508d91abb607435c027e8848f49a041
Bryan Souza <bryan.souza@fcamara.com.br>
2020-04-07 09:23:17 -0300

    add home

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,27 @@
 $(document).ready(() => {
 
-    /** MDC WEB COMPONENTS */
+    //
+    // CONSTANTS
+    //
+
+    const btnLogin = $("#login")
+    const btnRegister = $("#register")
+    const modal = $("div.backdrop")
+    const content = $("div.sideModal")
+    const textFieldPassword1 =  $("input[aria-labelledby='password1']")
+    const textFieldPassword2 =  $("input[aria-labelledby='password2']")
+    const passwordIcon1 = $("#secretPassword1")
+    const passwordIcon2 = $("#secretPassword2")
+    const checkbox = $('.mdc-checkbox')
+    const formField = $('.mdc-form-field')
+    const registerDiv = $('[data-form=register]')
+    const loginDiv = $('[data-form=login]')
+    const itemStatus = $('div.row.side-status-item')
+    $('[data-toggle="tooltip"]').tooltip()
+    
+    //
+    // CONFIG
+    //
 
     const textFields = $('.mdc-text-field');
 
@@ -8,25 +29,58 @@ $(document).ready(() => {
         mdc.textField.MDCTextField.attachTo(textField);
     }
 
-    const icons = $('.mdc-text-field-icon');
+    const iconButtons = $('.mdc-icon-button');
 
-    for (const icon of icons ) {
-        mdc.textField.icon.MDCTextFieldIcon.attachTo(icon);
+    for (const iconButton of iconButtons) {
+        mdc.iconButton.MDCIconButtonToggle.attachTo(iconButton)
     }
 
+    formField.input = checkbox
 
-    let btnLogin = $("button.login")
-    let btnRegister = $("button.register")
-    let modal = $("div.backdrop")
-    let content = $("div.sideModal")
+    //
+    // HANDLERSs
+    //
+
+    itemStatus.on("click", (event) => {
+        for (const item of itemStatus) {
+            $(item).removeClass("clicked")
+        }
+        $(event.target).addClass("clicked")
+        
+    })
+
+    passwordIcon1.on("click", (event) => {
+        if(passwordIcon1.text() == "visibility") {
+            passwordIcon1.text("visibility_off")
+            $(textFieldPassword1).attr("type","password")
+        } else {
+            passwordIcon1.text("visibility")
+            $(textFieldPassword1).attr("type","text")
+        }
+    })
+
+    passwordIcon2.on("click", (event) => {
+        if(passwordIcon2.text() == "visibility") {
+            passwordIcon2.text("visibility_off")
+            $(textFieldPassword2).attr("type","password")
+        } else {
+            passwordIcon2.text("visibility")
+            $(textFieldPassword2).attr("type","text")
+        }
+    })
 
     btnLogin.on("click", () => {
         modal.removeClass("active").addClass("active")
 
+        loginDiv.removeClass("invisible")
+        registerDiv.addClass("invisible")
     })
 
     btnRegister.on("click", () => {
         modal.removeClass("active").addClass("active")
+
+        registerDiv.removeClass("invisible")
+        loginDiv.addClass("invisible")
     })
 
 })
