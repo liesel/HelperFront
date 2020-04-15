49872032d7666ba137b4a6699b35916868523a23
Bryan Pinheiro de Souza <bryansouza_01@outlook.com>
2020-04-15 14:52:11 -0300

    fix css paths

--- src/app.js
+++ src/app.js
@@ -17,33 +17,12 @@ app.set('view engine', 'hbs')
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Andrew Mead'
-    })
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Andrew Mead'
-    })
-})
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Andrew Mead'
-    })
-})
-
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    })
+app.get('/home', (req, res) => {
+    res.render('home')
 })
 
 app.listen(3000, () => {
